18d4ec1f16e2c0332a43432c0c5f6bc0c1fb5631
Andrew Bednoff <andrcmdr@commandr.stream>
2020-11-19 04:49:50 +0600

    Additional example of usage IBCRunnable::run() method with first parameter as context of config path and second parameter as config file path

--- src/zcash_zebra_aslib.rs
+++ src/zcash_zebra_aslib.rs
@@ -1,3 +1,8 @@
+use std::path::{
+//  Path,
+    PathBuf,
+};
+
 use zebra_chain::{
     block::{
 //      Block,
@@ -31,18 +36,23 @@ pub trait Runnable {
 
 impl dyn Runnable {
     fn run(&self) {
-//      let context = IBCItems{ hash: BlockHeaderHash([0u8; 32]), height: BlockHeight(0) };
-//      let context = IBCItems{ hash: GENESIS, height: BlockHeight(0) };
-//      IBCRunnable::run(&context);
-        IBCRunnable::run(&IBCItems::default());
+//      let context = Config { path: PathBuf::from("./zebrad.toml") };
+        let filepath = PathBuf::from("./zebrad.toml");
+//      IBCRunnable::run(&context, None);
+//      IBCRunnable::run(&context, Some(filepath));
+//      IBCRunnable::run(&Config::default(), None);
+        IBCRunnable::run(&Config::default(), Some(filepath));
+
         async move {
             let _header_by_hash = IBCRequest::get(&IBCItems::default(), BlockHeaderHash([0u8; 32])).await;
             let _header_by_height = IBCRequest::get(&IBCItems::default(), BlockHeight(0)).await;
             let _tip = IBCRequest::get_tip(&IBCItems::default()).await;
 
+//          let context = IBCItems{ hash: BlockHeaderHash([0u8; 32]), height: BlockHeight(0) };
             let context = IBCItems{ hash: GENESIS, height: BlockHeight(0) };
             let _header_by_hash_ = IBCRequest::get(&context, GENESIS).await;
             let _header_by_height_ = IBCRequest::get(&context, BlockHeight(0)).await;
+            let _tip_ = IBCRequest::get_tip(&context).await;
         }.boxed();
     }
 }
