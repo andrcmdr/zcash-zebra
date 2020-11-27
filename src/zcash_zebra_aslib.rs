a7c02418f3f4e916e30e8e55901dd43a0672bf8d
Andrew Bednoff <andrcmdr@commandr.stream>
2020-11-27 07:15:12 +0600

    Renew examples of how to use a common state context (between zebra-state & IBCLib subsystems) from IBCLib using Tower Service for integration purposes, due to splitting IBCLib into two submodules: extracted IBCRun & separate it from IBCLib::IBCRequest

--- src/zcash_zebra_aslib.rs
+++ src/zcash_zebra_aslib.rs
@@ -2,7 +2,6 @@ use std::path::{
 //  Path,
     PathBuf,
 };
-
 use zebra_chain::{
     block::{
 //      Block,
@@ -11,15 +10,21 @@ use zebra_chain::{
     },
     types::BlockHeight,
 };
-
+use zebra_state::{
+//  QueryType,
+//  RequestBlock,
+    RequestBlockHeader,
+    Response,
+};
 use std::{
     error,
 //  error::Error,
 //  future::Future,
 };
-
 use futures_util::FutureExt;
+use tower::Service;
 
+use ibcrun::prelude::*;
 use ibclib::prelude::*;
 
 type Error = Box<dyn error::Error + Send + Sync + 'static>;
@@ -34,7 +39,12 @@ pub trait Runnable {
     fn run(&self);
 }
 
-impl dyn Runnable {
+impl<S> Runnable for Storage<S>
+where
+    S: Service<RequestBlockHeader, Response = Response, Error = Error> + Send + Clone + 'static,
+    S::Future: Send,
+    Self: Send + Sync + 'static,
+{
     fn run(&self) {
 //      let context = Config { path: PathBuf::from("./zebrad.toml") };
         let filepath = PathBuf::from("./zebrad.toml");
@@ -44,15 +54,10 @@ impl dyn Runnable {
         IBCRunnable::run(&Config::default(), Some(filepath));
 
         async move {
-            let _header_by_hash = IBCRequest::get(&IBCItems::default(), BlockHeaderHash([0u8; 32])).await;
-            let _header_by_height = IBCRequest::get(&IBCItems::default(), BlockHeight(0)).await;
-            let _tip = IBCRequest::get_tip(&IBCItems::default()).await;
-
-//          let context = IBCItems{ hash: BlockHeaderHash([0u8; 32]), height: BlockHeight(0) };
-            let context = IBCItems{ hash: GENESIS, height: BlockHeight(0) };
-            let _header_by_hash_ = IBCRequest::get(&context, GENESIS).await;
-            let _header_by_height_ = IBCRequest::get(&context, BlockHeight(0)).await;
-            let _tip_ = IBCRequest::get_tip(&context).await;
+            let _header_by_height = IBCRequest::get(self, BlockHeight(0)).await;
+            let _header_by_hash = IBCRequest::get(self, BlockHeaderHash([0u8; 32])).await;
+            let _header_by_hash_ = IBCRequest::get(self, GENESIS).await;
+            let _tip = IBCRequest::get_tip(self).await;
         }.boxed();
     }
 }
